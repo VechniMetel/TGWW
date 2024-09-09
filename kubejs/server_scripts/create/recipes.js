d9d2fc80942854b16fc354f641c4cb61d4aa5676
VechniMetel <3512546747@qq.com>
2024-09-09 21:38:28 +0800

    Update recipes.js

--- kubejs/server_scripts/create/recipes.js
+++ kubejs/server_scripts/create/recipes.js
@@ -3,5 +3,23 @@ ServerEvents.recipes(r =>{
     ["3x minecraft:bone_meal","minecraft:lime_dye"])
   r.recipes.create.deploying('minecraft:shulker_shell',
     ['minecraft:turtle_helmet','minecraft:popped_chorus_fruit'])
- })
- 
+  r.recipes.create.mixing("2x minecraft:andesite",
+    ["2x minecraft:cobblestone","minecraft:quartz"])
+  r.recipes.create.haunting(['create_netherless:ectoplasm',
+    Item.of('minecraft:string').withChance(0.1)],'minecraft:string')
+  r.recipes.create.compacting('minecraft:phantom_membrane',
+    ['minecraft:rotten_flesh','create_netherless:ectoplasm','minecraft:leather'])
+  r.recipes.create.mixing('minecraft:chorus_fruit',
+    ['minecraft:ender_pearl',"#forge:fruits/sweet"])
+  r.recipes.create.mixing('minecraft:chorus_flower',
+    ['minecraft:chorus_fruit',"#minecraft:small_flowers"])
+  r.recipes.create.mixing('minecraft:kelp',[Fluid.water(500),
+    'minecraft:seagrass','vintagedelight:salt_dust'])
+  r.recipes.create.haunting('minecraft:fern','minecraft:grass')
+  r.recipes.create.sandpaper_polishing('create_netherless:coal_rod',
+    "#minecraft:coals")
+  r.recipes.create.milling('crockpot:blackstone_dust','minecraft:blackstone')
+  r.recipes.create.deploying('minecraft:end_stone',['minecraft:sandstone','minecraft:popped_chorus_fruit'])
+  r.recipes.create.mixing('minecraft:ghast_tear',['minecraft:warped_fungus','create_netherless:ectoplasm',
+    Fluid.water(1000)])
+  })
