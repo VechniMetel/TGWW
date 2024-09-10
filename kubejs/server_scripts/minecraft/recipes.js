97dd4eeb0575284ccf23a024e68ebfb005054966
unknown <3512546747@qq.com>
2024-09-10 16:19:16 +0800

    aaa
    aaa

--- kubejs/server_scripts/minecraft/recipes.js
+++ kubejs/server_scripts/minecraft/recipes.js
@@ -1,3 +1,10 @@
 ServerEvents.recipes(r=>{
 r.remove({output:'constructionwand:core_destruction'})
+r.shapeless(Item.of('minecraft:red_mushroom',2),
+['#minecraft:logs','minecraft:shears','minecraft:sugar']
+).damageIngredient({item:'minecraft:shears'},2)
+r.shapeless(Item.of('minecraft:brown_mushroom',2),
+['minecraft:mud','minecraft:shears','minecraft:sugar']
+).damageIngredient({item:'minecraft:shears'},2)
+r.remove({output:Item.of(Item.of('tacz:ammo', '{AmmoId:"tacz:magnum_r"}'))})
 })
