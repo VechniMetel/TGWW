bd84fc1562ab7e36b4838a741c7f16ee83ff3d6b
unknown <3512546747@qq.com>
2024-09-24 17:51:14 +0800

    butchercraft

---
+++ kubejs/server_scripts/farmersdelight/values.js
@@ -0,0 +1,142 @@
+ServerEvents.recipes(r=>{
+    r.custom({
+        "type": "crockpot:food_values",
+        "items": [
+          "butchercraft:eyeball",
+          "butchercraft:cooked_eyeball"
+        ],
+        "values": {
+          "MONSTER": 0.75,
+          "MEAT": 0.25
+        }
+      })
+    r.custom({
+        "type": "crockpot:food_values",
+        "items": [
+          "butchercraft:cooked_oxtail",
+          "butchercraft:oxtail"
+        ],
+        "values": {
+          "MEAT": 0.5
+        }
+      })
+    r.custom({
+        "type": "crockpot:food_values",
+        "items": [
+          "butchercraft:cooked_tongue",
+          "butchercraft:tongue"
+        ],
+        "values": {
+          "MEAT": 0.5
+        }
+      })
+    r.custom({
+        "type": "crockpot:food_values",
+        "tags": [
+          "forge:offal",
+          "forge:stomach"
+        ],
+        "values": {
+          "MEAT": 0.5,
+          "MONSTER": 0.5
+        }
+      })
+    r.custom({
+        "type": "crockpot:food_values",
+        "tags": [
+          "forge:beef/stew",
+          "forge:pork/stew",
+          "forge:goat/stew",
+          "forge:mutton/stew"
+        ],
+        "values": {
+          "MEAT": 0.25
+        }
+      })
+    r.custom({
+        "type": "crockpot:food_values",
+        "tags": [
+          "forge:meat/scrap",
+          "forge:goat/scrap"
+        ],
+        "values": {
+          "MEAT": 0.25
+          
+        }
+      })
+    r.custom({
+        "type": "crockpot:food_values",
+        "items": [
+          "butchercraft:cooked_goat_chop",
+          "butchercraft:goat_chop"
+        ],
+        "values": {
+          "MEAT": 0.75
+        }
+      })
+    r.custom({
+        "type": "crockpot:food_values",
+        "tags": [
+          "forge:beef/cubed",
+          "forge:pork/cubed",
+          "forge:goat/cubed"
+        ],
+        "values": {
+          "MEAT": 0.5
+        }
+      })
+    r.custom({
+        "type": "crockpot:food_values",
+        "tags": [
+          "forge:ground_meat"
+        ],
+        "values": {
+          "MEAT": 0.25
+        }
+      })
+    r.custom({
+        "type": "crockpot:food_values",
+        "tags": [
+          "forge:chicken"
+        ],
+        "values": {
+          "MEAT": 0.25
+        }
+      })
+    r.custom({
+        "type": "crockpot:food_values",
+        "tags": [
+          "forge:rabbit"
+        ],
+        "values": {
+          "MEAT": 0.25
+        }
+      })
+    r.custom({
+        "type": "crockpot:food_values",
+        "tags": [
+          "forge:meat/roast"
+        ],
+        "values": {
+          "MEAT": 1.5
+        }
+      })
+    r.custom({
+        "type": "crockpot:food_values",
+        "tags": [
+          "forge:meat/ribs"
+        ],
+        "values": {
+          "MEAT": 1.5
+        }
+      })
+    r.custom({
+        "type": "crockpot:food_values",
+        "tags": [
+          "forge:mutton/cubed"
+        ],
+        "values": {
+          "MEAT": 0.25
+        }
+      })
+})
